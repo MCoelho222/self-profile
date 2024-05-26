c26f2b569602fe96b93a31a597a5065e56138f60
Marcelo Coelho <mcoelho2011@hotmail.com>
2024-05-26 19:28:56 -0300

    fix: add key to map in experience

--- app/ui/experience.tsx
+++ app/ui/experience.tsx
@@ -7,16 +7,16 @@ const Experience = ({ title, content }: ExperienceType) => {
             <p className="purple-gradient title">{title}</p>
             {content.map((item) => {
                 return (
-                <div className="flex flex-row w-full mb-8">
-                    <div className="w-1/6">
-                        <p className="dark-text">{item.period}</p>
+                    <div className="flex flex-row w-full mb-8" key={item.title}>
+                        <div className="w-1/6">
+                            <p className="dark-text">{item.period}</p>
+                        </div>
+                        <div className="w-5/6">
+                            <p className="main-text">{item.title} at <a className="purple-gradient" href={item.link} target="_blank" rel="noopener noreferrer">{item.company}</a></p>
+                            <p className="dark-text mb-4">{item.description}</p>
+                            <p className="dark-text">{item.tech.join(', ')}</p>
+                        </div>
                     </div>
-                    <div className="w-5/6">
-                        <p className="main-text">{item.title} at <a className="purple-gradient" href={item.link} target="_blank" rel="noopener noreferrer">{item.company}</a></p>
-                        <p className="dark-text mb-4">{item.description}</p>
-                        <p className="dark-text">{item.tech.join(', ')}</p>
-                    </div>
-                </div>
                 );
             })}
         </div>
