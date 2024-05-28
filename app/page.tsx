ff3ed0187c1bcf556ea987c6746ba0e33a55ca1b
Marcelo Coelho <mcoelho2011@hotmail.com>
2024-05-27 21:22:10 -0300

    feat: responsiveness improved

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ import Info from './ui/info';
 export default function Home() {
   const { cvData } = require('./lib/cv-data.js');
   return (
-    <main className="flex flex-row min-h-screen bg-slate-900 p-4 mx-5 justify-center">
-      <div className="fixed top-5 left-10 flex flex-col pt-10 pl-10">
+    <main className="flex flex-col justify-center min-h-screen bg-slate-900 p-4 mx-5 sm:flex-row">
+      <div className=" w-full flex flex-col items-center sm:items-start sm:fixed sm:top-5 sm:left-10 sm:pt-10 sm:pl-1 ">
         <Image
           className="h-36 w-36 rounded-full mb-10 border-2 border-solid border-slate-500"
           width={300}
@@ -18,15 +18,12 @@ export default function Home() {
         />
         <Info content={cvData.info}/>
       </div>
-      <div className="w-2/4 mt-24 ml-[30%]">
+      <div className="w-full mt-24 sm:w-2/4 ">
         
         {/* <p className="text-4xl text-slate-300 font-bold mb-36">Welcome to my Profile!</p> */}
         <AboutMe title='About me' content={cvData.aboutMe} />
         <Education title="Education" content={cvData.education} />
         <Experience title="DevExperience" content={cvData.dev_experience} />
-      </div>
-      <div className="w-1/4 flex flex-row justify-end">
-      
       </div>
     </main>
   );
