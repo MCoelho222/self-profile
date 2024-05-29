799b2590ed6dbb043853e02e13dc344738e56d2b
Marcelo Coelho <mcoelho2011@hotmail.com>
2024-05-29 13:50:54 -0300

    feat: smaller image

--- app/ui/certificates.tsx
+++ app/ui/certificates.tsx
@@ -6,24 +6,21 @@ const Certificates = ({ title, content }: CertificatesType) => {
             <p className="title blue-gradient">{title}</p>
             {content.map((item) => {
                 return (
-                <div className="flex flex-row w-full mb-4" key={item.title}>
+                <div className="flex flex-row w-full mb-4 justify-start" key={item.title}>
                     <div className="w-1/6">
                         <p className="dark-text">{item.year}</p>
                     </div>
-                    <div className="w-5/6">
-                        <div className="flex flex-row gap-2">
-                            <p className="main-text">{item.title}</p>
-                            <p className="dark-text">by</p>
-                            <p className="dark-text">{item.institution}</p>
-                            <p className="dark-text">- <a
-                                className="blue-link"
-                                href={item.link}
-                                target="_blank"
-                                rel="noopener noreferrer">
-                                    Certificate
-                            </a></p>
-                            
-                        </div>
+                    <div className="w-5/6 flex flex-row gap-2">
+                        <p className="main-text">{item.title}</p>
+                        <p className="dark-text">by</p>
+                        <p className="dark-text">{item.institution}</p>
+                        <p className="dark-text">- <a
+                            className="blue-link"
+                            href={item.link}
+                            target="_blank"
+                            rel="noopener noreferrer">
+                                Certificate
+                        </a></p>
                     </div>
                 </div>
                 );
