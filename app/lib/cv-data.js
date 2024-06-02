f0d9203ed1003e2eb9cdf5aca5faf7fd54de8793
Marcelo Coelho <mcoelho2011@hotmail.com>
2024-06-02 15:21:45 -0300

    feat: new welcome message

--- app/lib/cv-data.js
+++ app/lib/cv-data.js
@@ -9,9 +9,9 @@ const cvData = {
         titles: ['Environmental Engineer', 'Fullstack Developer', 'AI Specialist'],
     },
     welcome: "Hello, welcome to my profile! Here, you'll find the most important parts of my professional history. \
-    But speaking about me, I have a simple life style, I love being close to the nature, music and programming. \
-    I have a wife, two grown kids, live close to the beach, ...etc. I love to work remotely and my goal is to work with AI models, \
-    data analysis and web development.",
+    But speaking about me, I have a simple life style, love being close to the nature, music and programming. \
+    I have a wife, two grown kids, live close to the beach, love making BBQ on sundays. I really enjoy working remotely \
+    and my goal is to work with AI models, data analysis and web development.",
     aboutMe: 
     "I am a full-stack developer and environmental engineer with strong expertise in statistical data analysis. \
     I worked as a professor in higher education for undergraduate courses in Environmental Engineering from 2013 until 2021, \
