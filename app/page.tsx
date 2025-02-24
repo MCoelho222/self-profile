695554bae95a12f6156cf1489d02741912276549
Marcelo Coelho <mcoelho2011@hotmail.com>
2025-02-24 15:07:23 -0300

    feat: welcome text and styles updated

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ import Info from './ui/info';
 export default function Home() {
   const { cvData } = require('./lib/cv-data.js');
   return (
-    <main className="w-full flex flex-col justify-center min-h-screen bg-slate-200 p-2 mx-5 sm:flex-row">
-      <div className=" w-full h-full flex flex-col items-center sm:w-1/5 sm:fixed sm:top-0 sm:left-0 sm:pt-7 sm:px-4 sm:bg-slate-300">
+    <main className="flex flex-col justify-center min-h-screen bg-slate-200 p-5 sm:flex-row">
+      <div className="w-full h-full flex flex-col items-center sm:w-1/5 sm:fixed sm:top-0 sm:left-0 sm:pt-9 sm:px-7 sm:bg-slate-300">
         <Image
           className="h-32 w-32 rounded-full mb-2 border-2 border-solid border-slate-300 object-cover"
           width={300}
@@ -19,7 +19,7 @@ export default function Home() {
         />
         <Info content={cvData.info}/>
         <div className="flex flex-row justify-center pt-3">
-          <p className="text-slate-900 text-sm sm:text-base text-justify">{cvData.welcome}</p>
+          <p className="dark-text sm:text-base text-justify">{cvData.welcome}</p>
         </div>
       </div>
       <div className="w-full pt-8 sm:w-3/4 sm:pl-[10%]">
--- app/ui/info.tsx
+++ app/ui/info.tsx
@@ -4,16 +4,16 @@ import { BsLinkedin } from 'react-icons/bs';
 
 const Info = ({ title, content }: InfoType) => {
     return (
-        <div className="text-slate-800 text-sm">
-            <div className=" flex flex-col items-center mb-4">
-                <p className="text-xl text-slate-800 text-justify mb-4 font-medium">{content.name}</p>
+        <div className="text-slate-800 dark-text sm:text-base">
+            <div className="flex flex-col items-center mb-4">
+                <p className="text-xl text-slate-700 text-justify mb-4 mt-4 font-medium">{content.name}</p>
                 {content.titles.map((item) => {
                     return (
                         <p key="item">{item}</p>
                     )
                 })}
             </div>
-            <p className="mb-7">{content.email}</p>
+            <p className="dark-text sm:text-base mb-7">{content.email}</p>
             <div className="text-2xl flex flex-row gap-3 justify-center mb-4">
                 <a href={content.github} target="_blank" rel="noopener noreferrer"><GrGithub /></a>
                 <a href={content.linkedin} target="_blank" rel="noopener noreferrer"><BsLinkedin /></a> 
